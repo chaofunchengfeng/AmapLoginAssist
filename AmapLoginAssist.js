0f2f8fe7f075ad89e58b65a8f3cfa67a79f45249
chaofunchengfeng <chaofunchengfeng@qq.com>
2025-03-04 04:49:43 +0800

    fix: desc

--- AmapLoginAssist.js
+++ AmapLoginAssist.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         AmapLoginAssist - 高德地图支持密码登录、三方登录
-// @namespace    https://choa.fun/p/1282414
-// @homepage     https://choa.fun/p/1282414
-// @homepageURL  https://choa.fun/p/1282414
-// @website      https://choa.fun/p/1282414
-// @source       https://choa.fun/p/1282414
-// @version      0.4
-// @description  高德地图支持密码登录、三方登录
-// @author       cf
+// @name         AmapLoginAssist - 高德地图Web网页版支持密码登录、三方登录
+// @namespace    https://github.com/chaofunchengfeng/AmapLoginAssist
+// @homepage     https://github.com/chaofunchengfeng/AmapLoginAssist
+// @homepageURL  https://github.com/chaofunchengfeng/AmapLoginAssist
+// @website      https://github.com/chaofunchengfeng/AmapLoginAssist
+// @source       https://github.com/chaofunchengfeng/AmapLoginAssist
+// @version      0.5
+// @description  高德地图Web网页版支持密码登录、三方登录
+// @author       chaofunchengfeng
 // @match        https://*.amap.com/*
 // @icon         https://www.amap.com/favicon.ico
 // @run-at       document-idle
